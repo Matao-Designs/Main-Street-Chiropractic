28837ff0a2c123e8ad86c9c00b13182c194d5211
MataoDesigns <matt@mataodesigns.com>
2025-02-25 00:02:43 -0500

    to version 11

--- src/app/pages/meet-the-team/meet-the-team.component.spec.ts
+++ src/app/pages/meet-the-team/meet-the-team.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MeetTheTeamComponent } from './meet-the-team.component';
 
@@ -6,7 +6,7 @@ describe('MeetTheTeamComponent', () => {
   let component: MeetTheTeamComponent;
   let fixture: ComponentFixture<MeetTheTeamComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MeetTheTeamComponent ]
     })
